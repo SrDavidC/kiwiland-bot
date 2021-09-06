e72d58adaff855172b005b2771cd4452c77684f5
SrDavidC <82900350+SrDavidC@users.noreply.github.com>
2021-09-06 15:14:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,27 +16,10 @@ client.on("ready", async () => {
     client.user.setActivity("Kiwiland.wtf ", { type: "PLAYING" });
 });
 client.on("message", async message => {
-    // This event will run on every single message received, from any channel or DM.
-
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
     if (message.author.bot) return;
-
-    // Also good practice to ignore any message that does not start with our prefix, 
-    // which is set in the configuration file.
-    // `startsWith` was added in ES2015 and you can use this instead of
-    // if(message.content.indexOf(config.prefix) !== 0) return;
     if (!message.content.startsWith(config.prefix)) return;
-
-    // Here we separate our "command" name, and our "arguments" for the command. 
-    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
-    // args = ["Is", "this", "the", "real", "life?"]
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    // Let's go with a few common example commands! Feel free to delete or change those.
-    
+    const command = args.shift().toLowerCase(); 
     if(command ==="pef"){
         message.channel.send(":yum:")
     }
@@ -126,6 +109,42 @@ client.on("message", async message => {
             throw error;
         })
     }
+
+    else if (command === "anuncio") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('No tienes permisos para usar este comando');
+
+        let mention;
+
+        if (!args.length) return message.channel.send('> Usage: M!anuncio <#channel> <message> <-ping ?>');
+
+        const channel = message.mentions.channels.first();
+        if (!channel) return message.reply('Especifica el canal');
+
+        if (!args[1]) return message.reply('Especifica el mensaje');
+
+        // mentions
+        if (args.some((val) => val.toLowerCase() === '-ping')) {
+            for (let i = 0; i < args.length; i++) {
+                if (args[i].toLowerCase() === '-ping') args.splice(i, 1);
+            }
+
+            mention = true;
+        } else mention = false;
+
+        if (mention === true) channel.send('@everyone');
+
+        channel.send(
+            new MessageEmbed()
+                
+                .setTitle(" ➲__**ANUNCIO**__ ")
+                .setDescription(args.slice(1).join(" "))
+                .setColor('#c50118')
+        )
+
+
+    }
+
+
 })
 
 

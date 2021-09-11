fd50b2d17245f2fb5f6b82c3f5a19b1937aa4d5d
SrDavidC <82900350+SrDavidC@users.noreply.github.com>
2021-09-11 00:13:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ client.on("message", async message => {
                 
                 
                 .setDescription(args.slice(1).join(" "))
-                .setColor('#25AADB')
+                .setColor('#FFF83C')
         )
     }
 
